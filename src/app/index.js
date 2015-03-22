663e9b7768b4fd7be6801d48ccf293b84e5d8fca
Dan Shahin <dan@codescience.com>
2015-03-21 17:10:53 -0700

    add videungular plugins

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,14 @@
 'use strict';
 
-angular.module('materialMedia', ['ngAnimate', 'ngTouch', 'ngSanitize', 'ui.router', 'ngMaterial','com.2fdevs.videogular'])
+angular.module('materialMedia', ['ngAnimate', 'ngTouch', 'ngSanitize', 'ui.router', 'ngMaterial',
+
+								'com.2fdevs.videogular',
+								'com.2fdevs.videogular.plugins.buffering',
+								'com.2fdevs.videogular.plugins.controls',
+								'com.2fdevs.videogular.plugins.overlayplay',
+								'com.2fdevs.videogular.plugins.poster'
+
+	])
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('home', {
