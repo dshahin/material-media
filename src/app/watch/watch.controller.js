5e88c6d0970a081dc6cdf9b355064ea6a8d6fb4e
Dan Shahin <dan@codescience.com>
2015-03-22 23:01:20 -0700

    change media

--- src/app/watch/watch.controller.js
+++ src/app/watch/watch.controller.js
@@ -13,14 +13,8 @@ angular.module('materialMedia')
         WC.config = {
             preload: "none",
             sources: [{
-                src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"),
+                src: $sce.trustAsResourceUrl("/assets/video/Popeye_Nearlyweds_512kb.mp4"),
                 type: "video/mp4"
-            }, {
-                src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"),
-                type: "video/webm"
-            }, {
-                src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"),
-                type: "video/ogg"
             }],
             tracks: [{
                 src: "/assets/subs/pale-blue-dot.vtt",
